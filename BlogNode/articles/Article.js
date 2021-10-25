288c24ec24077af8a65a42bca05dfb893e8eafa2
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-10-25 16:23:15 -0300

    BD
    criar tabelas dentro do banco de dados e formar um relacionamento entre essas duas tabelas

--- BlogNode/articles/Article.js
+++ BlogNode/articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");
 
 const Article = connection.define('articles',{
     title:{
@@ -16,5 +17,12 @@ const Article = connection.define('articles',{
     }
         
 })
+
+Category.hasMany(Article); // Uma categoria tem muitos artigos, relacionamento
+Article.belongsTo(Category);// O artigo pertence a uma categoria, relacionamento 1 para 1
+
+/*Article.sync({force:true}); Essa linha aqui serve para criar a tabela no banco de dados, depois 
+que for rodada uma vez é bom retirar ou comentar essa linha para o codigo nao ficar tentando criar
+as tabelas toda vez que executar*/ 
 
 module.exports = Article;
