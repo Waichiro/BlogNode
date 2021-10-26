62303272afd6419ffb496ea8fa599b6d971b0ab0
Gabriel Santos <gabrielsantos98898@gmail.com>
2021-10-26 11:05:48 -0300

    ControllerArticles
    Rota de salvar categorias, ajustes no navbar, adicao do conteudo no banco de dados para o ejs

--- BlogNode/articles/ArticlesController.js
+++ BlogNode/articles/ArticlesController.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-    res.send("Rota artigos")
-})
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{model: Category}] // inclui os dados do Category
+    }).then(articles => {
+        res.render("admin/articles/index", {articles: articles});
+    })
+    
+});
 
+//rota para criar novo artigo
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories});
     });
     
 })
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles");
+    })
+});
  
 
 
